fa758ff005a208bfbe1996077f0490f3d635ca5c
Alker <DKS0519513@dcsg.com>
2019-02-22 17:11:14 -0500

    added publish deploy and patron service

---
+++ Components/GildedRose.Entities/operations/Patron.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using GildedRose.Core.Contracts;
+
+namespace GildedRose.Entities.Operations
+{
+    public class Patron : IAuditable
+    {
+        public Guid Identifier { get; set; }
+
+        public string Name { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public string CategoryName { get; set; }
+
+        public int ShelfLife { get; set; }
+
+        public bool IsLegendary { get; set; }
+
+        public DateTime StockDate { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public int CreatedBy { get; set; }
+
+        public DateTime? Modified { get; set; }
+
+        public int? ModifiedBy { get; set; }
+    }
+}
---
+++ Database/Platform/Tables/Patrons.sql
@@ -0,0 +1,26 @@
+﻿CREATE TABLE [operations].[Patrons]
+(
+    [Identifier] UNIQUEIDENTIFIER NOT NULL, 
+    [FirstName] NVARCHAR(100) NOT NULL, 
+	[LastName] NVARCHAR(100) NOT NULL, 
+    [Address1] [Address] NOT NULL, 
+	[Address2] [Address] NOT NULL, 
+	[City] [City] NOT NULL,
+	[State] [State] NOT NULL,
+	[PostalCode] [PostalCode] NOT NULL,
+	[Phone] [Phone] NOT NULL,
+	[Email] [Email] NOT NULL,
+	[IsDeleted] BIT NOT NULL,
+	[Created] [AuditDate] NOT NULL DEFAULT getutcdate(),
+    [CreatedBy] [AuditUser] NOT NULL, 
+	[Modified] [AuditDate] NULL,
+    [ModifiedBy] [AuditUser] NULL, 
+    CONSTRAINT [PK_PatronIdentifier] PRIMARY KEY CLUSTERED ([Identifier] ASC) on [operations],
+);
+
+GO
+
+CREATE UNIQUE NONCLUSTERED INDEX [UIX_inventory_item_name]
+    ON [operations].[Patrons]([LastName], [FirstName] ASC)
+    ON [operations];
+GO
